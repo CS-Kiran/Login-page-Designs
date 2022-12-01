9f57508602b090b552094bbc50fecf0c0b162868
Code@Kiran <109469540+CS-Kiran@users.noreply.github.com>
2022-12-01 19:00:14 +0530

    Update register.php

--- Video Background/register.php
+++ Video Background/register.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$conn = pg_connect("host=localhost dbname=project user=postgres password=kiran") or die("Unable to connect to database");
+$conn = pg_connect("host=localhost dbname=postgres user=postgres password=postgres") or die("Unable to connect to database");
 if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['pass']) && isset($_POST['submit2']))
 {
     $_SESSION['email']=$_POST['email'];
